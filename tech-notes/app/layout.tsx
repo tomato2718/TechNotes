87155759d408c4d4e67b61461f11268bcbc5c3a6
tomato2718 <qweasdzxc030@gmail.com>
2023-10-06 18:46:34 +0800

    Feat: 導覽列初稿

--- tech-notes/app/layout.tsx
+++ tech-notes/app/layout.tsx
@@ -1,23 +1,26 @@
-import './globals.css'
+import Image from 'next/image'
+
 import type { Metadata } from 'next'
+import type {
+  ReactElement,
+  ReactNode
+} from 'react'
+
+import '../globals/globals.css'
+
 
 export const metadata: Metadata = {
   title: 'Testing',
   description: 'Testing...',
 }
 
-export default function RootLayout({children}: {children: React.ReactNode}){
+export default function RootLayout({children}: {children: ReactNode}){
   return (
     <html lang="en">
       <body className='h-screen'>
-        <div className='sticky top-0 z-50 w-full h-16 px-12 py-4 debug'>
-          <div className='flex flex-row justify-between debug'>
-            <div>logo</div>
-            <div className=''>navbar</div>
-          </div>
-        </div>
+        <Nav/>
         <div className='flex flex-row h-full'>
-          <div className='basis-1/5 h-full px-12 debug'>
+          <div className='basis-1/5 h-full px-12'>
             <div className='h-full debug'>
               left bar
             </div>
@@ -26,5 +29,30 @@ export default function RootLayout({children}: {children: React.ReactNode}){
         </div>
       </body>
     </html>
+  )
+}
+
+function Nav(): ReactElement{
+  return (
+      <div className='sticky top-0 z-50 w-full px-12 py-2 bg-inherit opacity-95 border-b-2 border-sky-900/10'>
+        <div className='flex flex-row justify-between'>
+          <div className='flex flex-row gap-2 items-center'>
+            <Image
+              src='/tomato.png'
+              height={40}
+              width={40}
+              alt='tomato'
+            />
+            <span className='text-lg font-bold'>Tomato2718 的技術筆記</span>
+          </div>
+          <nav className='flex flex-row h-auto' id='Navbar'>
+            <ul className='flex flex-row gap-2 h-full'>
+              <li><a href="#">首頁</a></li>
+              <li><a href="#">GitHub</a></li>
+              <li><a href="#">聯絡我</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
   )
 }
